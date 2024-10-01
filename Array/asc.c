956173f20db906c4afaf5cf3e3f649f709c20eb5
HridyaDEV <hridyadev31@gmail.com>
2024-10-01 17:07:38 +0530

    Array programs

---
+++ Array/asc.c
@@ -0,0 +1,44 @@
+
+
+// program to print array in ascending order
+
+#include <stdio.h>
+
+int main() {
+    int n,i;
+
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n]; 
+
+    
+    printf("Enter %d elements of the array:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]); 
+    }
+
+    
+    printf("The array values are:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+ 
+    }
+ for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+printf("Array in Ascending Order: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
